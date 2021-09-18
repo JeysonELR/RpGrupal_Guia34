2f9e8aa8713d492be66298b5c3b1c4d465d51a32
JeysonELR <50606205+JeysonELR@users.noreply.github.com>
2021-09-18 11:39:58 -0500

    Informe técnico
    TODO1 :  
    
    Se emplea el algoritmo burbuja, donde se emplea el .size para determinar el área a ordenar por los siguientes for e if características del algoritmo.  
    
      
    
    TODO2: 
    
    Se emplea el algoritmo selección, donde se emplea el .size  nuevamente para determinar el área a ordenar por los siguientes for e if características del algoritmo.  Las ultimas líneas del código mediante set, actualiza la información de las variables de posición y del array "aux". 
    
      
    
    TODO3: 
    
    Se emplea el algoritmo burbuja, donde se emplea el .size() para determinar el área a ordenar por los siguientes for e if características del algoritmo.  A diferencia de ya que se emplea en un tipo cadena, es necesario emplear un .compareTo() dentro de un condicional para actualizar el contenido de las variables y lista "aux". 
    
      
    
    TODO4:  
    
    Se emplea el algoritmo inserción, donde se emplea el .size para determinar el área a ordenar por los siguientes for y while característicos del algoritmo. Fuera del for se ajusta el valor de posición, cual se emplea en la otra función, por el resultado del algoritmo contenido en el "aux".  
    
      
    
    TODO5: 
    
    Para esta funcionabilidad se emplea las condiciones for e if para que se haga una comparación por los caracteres "nombre" indicados, con los objetos de este tipo de la lista almacenada en aspirantes. 
    
      
    
    TODOS6: 
    
    Para emplear el algoritmo de búsqueda binaria. Se declaran diferentes variables que estarán dentro de las condicionales while, if, else if  y un else. Dónde según las condiciones se almacena el único valor no descartado desde el principio y final hasta el medio.  
    
      
    
    TODO7:  
    
    Para este caso se emplea la comparativa para cada uno de los elementos de la lista aspirantes, por lo que se emplea un for y luego un while, no sin antes ir almacenando los elementos de la lista en otra lista temporal "aux".  Ya dentro de esta otra lista, se condiciona para que siga recorriéndose hasta encontrar el menor valor "edad". Una vez terminada la segunda condición la primera almacería también el valor de "aux" en posición cual es empleada de en otra clase.  
    
      
    
    TODO8: 
    
    Para este caso se emplea la comparativa para cada uno de los elementos de la lista aspirantes, por lo que se emplea un for y luego un while, no sin antes ir almacenando los elementos de la lista en otra lista temporal "aux".  Ya dentro de esta otra lista, se condiciona para que siga recorriéndose hasta encontrar el mayor valor "edad". Una vez terminada la segunda condición la primera almacería también el valor de "aux" en posición cual es empleada de en otra clase.  
    
      
    
    TODO: 
    
    Para este caso se emplea la comparativa para cada uno de los elementos de la lista aspirantes, por lo que se emplea un for y luego un while, no sin antes ir almacenando los elementos de la lista en otra lista temporal "aux".  Ya dentro de esta otra lista, se condiciona para que siga recorriendose hasta encontrar el menor valor de "darAniosDeExperiencia". Una vez terminada la segunda condición la primera almacería también el valor de "aux" en posición cual es empleada de en otra clase.  
    
      
    
    TODO9: 
    
    Se declaran diferentes variables para emplear dentro de un for y un if. El for hace un contador que permite al if hacer una búsqueda, dónde a la que se asemeje a la recibida, esta será removida. Para luego asimilar el valor de “cont” para eliminados, cual es la variable que se convoca dentro del Main.  
    
      
    
    TODO10: 
    
    Se declaran diferentes variables para emplear dentro de un for y un if. El for hace un contador que permite al if hacer una búsqueda, dónde a la que se asemeje a la recibida, este objeto booleano dentro de la lista será cambiado por un true.

--- Actividad1/src/universidadean/empleo/mundo/BolsaDeEmpleo.java
+++ Actividad1/src/universidadean/empleo/mundo/BolsaDeEmpleo.java
@@ -1,7 +1,7 @@
 /**
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Universidad Ean (Bogot· - Colombia)
- * Departamento de TecnologÌa de la InformaciÛn y Comunicaciones
+ * Universidad Ean (Bogot√° - Colombia)
+ * Departamento de Tecnolog√≠a de la Informaci√≥n y Comunicaciones
  * Licenciado bajo el esquema Academic Free License version 2.1
  * <p>
  * Basado en un Proyecto Cupi2 (http://cupi2.uniandes.edu.co)
@@ -18,7 +18,7 @@ import java.util.Objects;
  * Es la clase que se encarga de manejar y organizar los aspirantes <br>
  * <b>inv: </b> <br>
  * aspirantes != null <br>
- * En el vector de aspirantes no hay dos o m·s con el mismo nombre
+ * En el vector de aspirantes no hay dos o m√°s con el mismo nombre
  */
 public class BolsaDeEmpleo {
     // -----------------------------------------------------------------
@@ -42,7 +42,7 @@ public class BolsaDeEmpleo {
     }
 
     // -----------------------------------------------------------------
-    // MÈtodos
+    // M√©todos
     // -----------------------------------------------------------------
 
     /**
@@ -59,12 +59,12 @@ public class BolsaDeEmpleo {
      * Agrega un nuevo aspirante a la bolsa
      *
      * @param nombreA           El nombre del aspirante - nombreA != null
-     * @param profesionA        La profesiÛn del aspirante - profesionA es uno de estos { ADMINISTRADOR, INGENIERO_INDUSTRIAL, CONTADOR, ECONOMISTA }
-     * @param aniosExperienciaA AÒos de experiencia del aspirante - aniosExperienciaA > 0
+     * @param profesionA        La profesi√≥n del aspirante - profesionA es uno de estos { ADMINISTRADOR, INGENIERO_INDUSTRIAL, CONTADOR, ECONOMISTA }
+     * @param aniosExperienciaA A√±os de experiencia del aspirante - aniosExperienciaA > 0
      * @param edadA             La edad del aspirante - edadA > 0
-     * @param telefonoA         El telÈfono del aspirante - telefonoA != null
+     * @param telefonoA         El tel√©fono del aspirante - telefonoA != null
      * @param imagenA           La ruta a la imagen del aspirante - imagenA != null
-     * @return Se retornÛ true si el aspirante fue adicionado o false de lo contrario
+     * @return Se retorn√≥ true si el aspirante fue adicionado o false de lo contrario
      */
 
     public boolean agregarAspirante(String nombreA, String profesionA, int aniosExperienciaA, int edadA, String telefonoA, String imagenA) {
@@ -75,22 +75,20 @@ public class BolsaDeEmpleo {
             aspirantes.add(nuevoAspirante);
             agregado = true;
         }
-
         return agregado;
     }
 
     /**
      * Organiza la lista de aspirantes por nombre usando el algoritmo de burbuja. <br>
-     * <b>post: </b>La lista de aspirantes est· ordenada por nombre (orden ascendente).
+     * <b>post: </b>La lista de aspirantes est√° ordenada por nombre (orden ascendente).
      */
     public void ordenarPorNombre() {
         // TODO: Realizar el ejercicio correspondiente
 
-
         System.out.println("Los nombres de los aspirantes en orden son :");
         String temp;
         int area = aspirantes.size();
-        for (int h = 0; h < area; h++) {          // Gestionando el proceso de los elementos hasta los m·s pesados.
+        for (int h = 0; h < area; h++) {          // Gestionando el proceso de los elementos hasta los m√°s pesados.
             for (int i = h + 1 ; i < area; i++) {  // Ordena los elementos gestionados.
                 if (aspirantes.get(h).darNombre().compareTo(aspirantes.get(i).darNombre())> 0) {
                     Aspirante aux = aspirantes.get(i);
@@ -99,24 +97,19 @@ public class BolsaDeEmpleo {
                 }
             }
         }
-
-
-
-
-
-
     }
 
     /**
-     * Organiza la lista de aspirantes por edad usando el algoritmo de selecciÛn <br>
-     * <b>post: </b>La lista de aspirantes est· ordenada por edad
+     * Organiza la lista de aspirantes por edad usando el algoritmo de selecci√≥n <br>
+     * <b>post: </b>La lista de aspirantes est√° ordenada por edad
      */
     public void ordenarPorEdad() {
         // TODO: Realizar el ejercicio correspondiente
-        for (int i = 0; i < aspirantes.size() - 1; ++i) {
+        int area = aspirantes.size();
+        for (int i = 0; i < area - 1; ++i) {
             int menor = Integer.MAX_VALUE;
             int posMenor = -1;
-            for (int j = i ; j < aspirantes.size(); ++j) {
+            for (int j = i ; j < area; ++j) {
                 if (aspirantes.get(j).darEdad() < menor){
                     menor = aspirantes.get(j).darEdad();
                     posMenor = j;
@@ -126,24 +119,34 @@ public class BolsaDeEmpleo {
             aspirantes.set(i, aspirantes.get(posMenor));
             aspirantes.set(posMenor, aux);
         }
-
     }
 
     /**
-     * Organiza la lista de aspirantes por profesiÛn usando el algoritmo de burbuja <br>
-     * <b>post: </b>El conjunto de aspirantes esta ordenado por profesiÛn
+     * Organiza la lista de aspirantes por profesi√≥n usando el algoritmo de burbuja <br>
+     * <b>post: </b>El conjunto de aspirantes esta ordenado por profesi√≥n
      */
     public void ordenarPorProfesion() {
         // TODO: Realizar el ejercicio correspondiente
+    
+    int area = aspirantes.size();
+            for (int h = 0; h < area; h++) {          // Gestionando el proceso de los elementos hasta los m√°s pesados.
+                for (int i = h + 1 ; i < area; i++) {  // Ordena los elementos gestionados.
+                    if (aspirantes.get(h).darProfesion().compareTo(aspirantes.get(i).darProfesion())> 0) {
+                        Aspirante aux = aspirantes.get(i);
+                        aspirantes.set(i, aspirantes.get(h));
+                        aspirantes.set(h, aux);
+                    }
+                }
+            }
+        }
     }
 
     /**
-     * Organiza la lista de aspirantes por aÒos de experiencia usando el algoritmo de inserciÛn <br>
-     * <b>post: </b>La lista de aspirantes est· ordenada por aÒos de experiencia
+     * Organiza la lista de aspirantes por a√±os de experiencia usando el algoritmo de inserci√≥n <br>
+     * <b>post: </b>La lista de aspirantes est√° ordenada por a√±os de experiencia
      */
     public void ordenarPorAniosDeExperiencia() {
         // TODO: Realizar el ejercicio correspondiente
-
 
         for ( int i = 0; i < aspirantes.size(); i++) {
                 int posicion = i;
@@ -153,19 +156,17 @@ public class BolsaDeEmpleo {
 
                    aspirantes.set(posicion, aspirantes.get(posicion-1)).darAniosExperiencia();
                    posicion--;
-        }
-
+                }
                 aspirantes.set(posicion, aux);
-    }
-
+        }
     }
 
 
     /**
-     * Busca un Aspirante seg˙n su nombre y retorna la posiciÛn en la que se encuentra. <br>
+     * Busca un Aspirante seg√∫n su nombre y retorna la posici√≥n en la que se encuentra. <br>
      *
      * @param nombre El nombre del aspirante buscado - nombre!=null
-     * @return Se retornÛ la posiciÛn donde se encuentra un aspirante con el nombre dado. Si no se encuentra ning˙n aspirante con ese nombre se retornÛ -1.
+     * @return Se retorn√≥ la posici√≥n donde se encuentra un aspirante con el nombre dado. Si no se encuentra ning√∫n aspirante con ese nombre se retorn√≥ -1.
      */
     public int buscarAspirante(String nombre) {
         int posicion = -1;
@@ -185,11 +186,11 @@ public class BolsaDeEmpleo {
     }
 
     /**
-     * Busca un aspirante utilizando una b˙squeda binaria. <br>
+     * Busca un aspirante utilizando una b√∫squeda binaria. <br>
      * <b>pre: </b> La lista de aspirantes se encuentra ordenada por nombre. <br>
      *
      * @param nombre es el nombre del aspirante que se va a buscar - nombre!=null
-     * @return Se retornÛ la posiciÛn del aspirante con el nombre dado. Si el aspirante no existe se retornÛ -1.
+     * @return Se retorn√≥ la posici√≥n del aspirante con el nombre dado. Si el aspirante no existe se retorn√≥ -1.
      */
     public int buscarBinarioPorNombre(String nombre) {
         int posicion = -1;
@@ -206,15 +207,13 @@ public class BolsaDeEmpleo {
                fin =  posicion-1;
            }
        }
-
         return posicion;
     }
-
 
     /**
      * Busca el aspirante que tenga la menor edad en la bolsa.
      *
-     * @return Se retornÛ la posiciÛn donde se encuentra el aspirante m·s joven. Si no hay aspirantes en la bolsa se retornÛ -1
+     * @return Se retorn√≥ la posici√≥n donde se encuentra el aspirante m√°s joven. Si no hay aspirantes en la bolsa se retorn√≥ -1
      */
     public int buscarAspiranteMasJoven() {
         int posicion = -1;
@@ -233,14 +232,12 @@ public class BolsaDeEmpleo {
             aspirantes.set(posicion, aux);
         }
         return posicion;
-
-
     }
 
     /**
      * Busca el aspirante que tenga la mayor edad en la bolsa.
      *
-     * @return Se retornÛ la posiciÛn donde se encuentra el aspirante con m·s edad. Si no hay aspirantes en la bolsa se retornÛ -1
+     * @return Se retorn√≥ la posici√≥n donde se encuentra el aspirante con m√°s edad. Si no hay aspirantes en la bolsa se retorn√≥ -1
      */
     public int buscarAspiranteMayorEdad() {
         int posicion = -1;
@@ -257,29 +254,36 @@ public class BolsaDeEmpleo {
             }
             aspirantes.set(posicion, aux);
         }
-
         return posicion;
     }
 
     /**
-     * Busca el aspirante con m·s aÒos de experiencia en la bolsa.
+     * Busca el aspirante con m√°s a√±os de experiencia en la bolsa.
      *
-     * @return Se retornÛ la posiciÛn donde se encuentra el aspirante con mayor experiencia. Si no hay aspirantes en la bolsa se retornÛ -1
+     * @return Se retorn√≥ la posici√≥n donde se encuentra el aspirante con mayor experiencia. Si no hay aspirantes en la bolsa se retorn√≥ -1
      */
     public int buscarAspiranteMayorExperiencia() {
         int posicion = -1;
-
         // TODO: Realizar el ejercicio correspondiente
-
+        for (int i = 0; i < aspirantes.size(); i++) {
+            posicion = i;
+            Aspirante aux = aspirantes.get(i);
+            while ((posicion > 0) && (aspirantes.get(posicion - 1).darAniosExperiencia()) <
+                    aux.darAniosExperiencia()) {
+                aspirantes.set(posicion, aspirantes.get(posicion - 1)).darAniosExperiencia();
+                posicion--;
+            }
+            aspirantes.set(posicion, aux);
+        }
         return posicion;
     }
 
     /**
      * Contrata a un aspirante.<br>
-     * <b>post: </b>Se eliminÛ el aspirante de la lista de aspirantes.
+     * <b>post: </b>Se elimin√≥ el aspirante de la lista de aspirantes.
      *
      * @param nombre El nombre del aspirante a contratar - nombre!=null
-     * @return Se retornÛ true si el aspirante estaba registrado en la bolsa o false de lo contrario
+     * @return Se retorn√≥ true si el aspirante estaba registrado en la bolsa o false de lo contrario
      */
     public boolean contratarAspirante(String nombre) {
         boolean contratado = false;
@@ -292,18 +296,15 @@ public class BolsaDeEmpleo {
             if (Objects.equals(nombre, aux.darNombre())){
                 contratado= true;
             }
-
         }
-
-
         return contratado;
     }
 
     /**
-     * Elimina todos los aspirantes de la bolsa cuyos aÒos de experiencia <br>
-     * son menores a la cantidad de aÒos especificada <br>
+     * Elimina todos los aspirantes de la bolsa cuyos a√±os de experiencia <br>
+     * son menores a la cantidad de a√±os especificada <br>
      *
-     * @param aniosExperiencia La cantidad de aÒos con relaciÛn a la cual se van a eliminar los aspirantes. aniosExperiencia>=0
+     * @param aniosExperiencia La cantidad de a√±os con relaci√≥n a la cual se van a eliminar los aspirantes. aniosExperiencia>=0
      * @return La cantidad de aspirantes que fueron eliminados
      */
     public int eliminarAspirantesPorExperiencia(int aniosExperiencia) {
@@ -321,5 +322,4 @@ public class BolsaDeEmpleo {
         }
         return eliminados;
     }
-
 }
